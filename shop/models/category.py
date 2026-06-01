7cde4b99fe4cbb0bff1cddc5f6bb482c90ebef58
akkaziev_aktan <salizardfnite@gmail.com>
2026-06-01 16:59:21 +0600

    slightly enhanced frontend part

--- shop/models/category.py
+++ shop/models/category.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    description = models.TextField(max_length=500, null=True, blank=True)
+    name = models.CharField(max_length=100, unique=True, verbose_name="Название")
+    description = models.TextField(max_length=500, null=True, blank=True, verbose_name="Описание")
 
     def __str__(self):
         return self.name
--- shop/urls.py
+++ shop/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from shop.views import products_view, product_add_view, product_view
+from shop.views import products_view, product_add_view, product_view, category_add_view
+
 
 urlpatterns = [
     path('', products_view, name='main'),
     path('products/', products_view, name='products'),
 
     path('products/add/', product_add_view, name='add_product'),
+    path('categories/add/', category_add_view, name='add_category'),
 
     # path('products/delete_task/<int:pk>/', delete_product, name='delete_product'),
 
