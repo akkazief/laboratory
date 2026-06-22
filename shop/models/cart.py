7dfc15a055ba9decb8259df58492782b2ec3d61c
akkaziev_aktan <salizardfnite@gmail.com>
2026-06-22 14:38:37 +0600

    added cart model, __str__ taken from sites

---
+++ shop/models/cart.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+from models import Product
+
+class Cart(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.RESTRICT, related_name='product')
+    amount = models.PositiveIntegerField(verbose_name="Количество", default=1)
+
+    def __str__(self):
+        return f"{self.product.name} x {self.amount} "
+
+    class Meta:
+        db_table = "cart_items"
+        verbose_name = "Товар в корзине"
+        verbose_name_plural = "Товары в корзине"
