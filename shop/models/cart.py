4fb3265a5990473ee650dce08d7caed88d2f0fc1
akkaziev_aktan <salizardfnite@gmail.com>
2026-06-22 14:41:15 +0600

    made migrations

--- shop/models/cart.py
+++ shop/models/cart.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from models import Product
+from shop.models import Product
 
 class Cart(models.Model):
     product = models.ForeignKey(Product, on_delete=models.RESTRICT, related_name='product')
